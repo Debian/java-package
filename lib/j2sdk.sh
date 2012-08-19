d7060e07977007ffbf5199efaf69e267febc999b
Cédric Pineau <cedric.pineau@gmail.com>
2012-08-19 14:04:52 +0200

    Change installation directory to be jdk|jre-relase.update-vendor-arch

--- lib/j2sdk.sh
+++ lib/j2sdk.sh
@@ -29,7 +29,7 @@ j2sdk_run() {
     diskfree "$j2se_required_space"
     read_maintainer_info
     j2se_package="$j2se_vendor-java$j2se_release-jdk"
-    j2se_name="jdk$j2se_release-$j2se_vendor"
+    j2se_name="jdk-$j2se_version-$j2se_vendor-$j2se_arch"
     local target="$install_dir$jvm_base$j2se_name"
     install -d -m 755 "$( dirname "$target" )"
     extract_bin "$archive_path" "$j2se_expected_min_size" "$target"
